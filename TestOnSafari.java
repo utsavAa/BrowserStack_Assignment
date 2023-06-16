b1ae688df42d09b453e4ef464564e6193cf24411
utsav-browserstack <130458371+utsav-browserstack@users.noreply.github.com>
2023-06-16 16:01:49 +0530

    Add test scripts

---
+++ TestOnSafari.java
@@ -0,0 +1,63 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+//import org.junit.Assert;
+
+
+public class TestOnSafari {
+	String username = System.getenv("USERNAME");
+    String password = System.getenv("PASSWORD");
+	public static void main(String[] args){
+		
+//		System.setProperty("webdriver.chrome.driver", "/Users/utsavbanerjee/Downloads/chromedriver 2");
+
+		//Creating WebDriver instance
+		WebDriver driver = new SafariDriver();
+		
+		//Navigate to web page
+		driver.get("https://www.browserstack.com/users/sign_in");
+		
+		//Maximizing window
+		driver.manage().window().maximize();
+		
+//		Retrieving web page title
+		String title = driver.getTitle();
+		System.out.println("The page title is : " +title);
+////		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//		
+//		//Locating web element
+		WebElement uName = driver.findElement(By.id("user_email_login"));
+		WebElement pswd = driver.findElement(By.id("user_password"));
+		WebElement loginBtn1 = driver.findElement(By.id("user_submit"));
+//		
+//
+//
+////		//Peforming actions on web elements
+				uName.sendKeys("username");
+				pswd.sendKeys("password");
+//				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+				loginBtn1.click();
+				
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//				
+//				
+//
+//				String actualUrl="https://live.browserstack.com/dashboard";
+//				String expectedUrl= driver.getCurrentUrl();
+////			
+//				System.out.println("Resulting page is: " +expectedUrl);
+//
+////				Assert.assertEquals(expectedUrl,actualUrl);
+//				
+//				//Closing browser session
+//				driver.quit();
+	}
+}
---
+++ TestSDK.java
@@ -0,0 +1,129 @@
+package test;
+
+
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+public class TestSDK {
+
+	@Test
+	public void testOnFirefox() {
+		String username = System.getenv("USERNAME");
+        String password = System.getenv("PASSWORD");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.browserstack.com/users/sign_in");
+		driver.manage().window().maximize();
+
+		String title = driver.getTitle();
+		System.out.println("The page title is: " + title);
+
+		WebElement uName = driver.findElement(By.id("user_email_login"));
+		WebElement pswd = driver.findElement(By.id("user_password"));
+		WebElement loginBtn1 = driver.findElement(By.id("user_submit"));
+
+		uName.sendKeys("username");
+		pswd.sendKeys("password");
+		loginBtn1.click();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+  		driver.quit();
+	}
+
+//	@Test
+//	public void testOnChrome() {
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("https://www.browserstack.com/users/sign_in");
+//		driver.manage().window().maximize();
+//
+//		String title = driver.getTitle();
+//		System.out.println("The page title is: " + title);
+//
+//		WebElement uName = driver.findElement(By.id("user_email_login"));
+//		WebElement pswd = driver.findElement(By.id("user_password"));
+//		WebElement loginBtn1 = driver.findElement(By.id("user_submit"));
+//
+//		uName.sendKeys("username");
+//		pswd.sendKeys("password");
+//		loginBtn1.click();
+//
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//  		driver.quit();
+//	}
+//
+//	@Test
+//	public void testOnSafari() {
+//		WebDriver driver = new SafariDriver();
+//		driver.get("https://www.browserstack.com/users/sign_in");
+//		driver.manage().window().maximize();
+//
+//		String title = driver.getTitle();
+//		System.out.println("The page title is: " + title);
+//
+//		WebElement uName = driver.findElement(By.id("user_email_login"));
+//		WebElement pswd = driver.findElement(By.id("user_password"));
+//		WebElement loginBtn1 = driver.findElement(By.id("user_submit"));
+//
+//		uName.sendKeys("username");
+//		pswd.sendKeys("password");
+//		loginBtn1.click();
+//
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//  		driver.quit();
+//	}
+//	
+//	@Test
+//	public void testOnEdge() {
+//		WebDriver driver = new EdgeDriver();
+//		driver.get("https://www.browserstack.com/users/sign_in");
+//		driver.manage().window().maximize();
+//
+//		String title = driver.getTitle();
+//		System.out.println("The page title is: " + title);
+//
+//		WebElement uName = driver.findElement(By.id("user_email_login"));
+//		WebElement pswd = driver.findElement(By.id("user_password"));
+//		WebElement loginBtn1 = driver.findElement(By.id("user_submit"));
+//
+//		uName.sendKeys("username");
+//		pswd.sendKeys("password");
+//		loginBtn1.click();
+//
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//  		driver.quit();
+//	}
+//	
+//	@Test
+//	public void testOnIE() {
+//		WebDriver driver = new InternetExplorerDriver();
+//		driver.get("https://www.browserstack.com/users/sign_in");
+//		driver.manage().window().maximize();
+//
+//		String title = driver.getTitle();
+//		System.out.println("The page title is: " + title);
+//
+//		WebElement uName = driver.findElement(By.id("user_email_login"));
+//		WebElement pswd = driver.findElement(By.id("user_password"));
+//		WebElement loginBtn1 = driver.findElement(By.id("user_submit"));
+//
+//		uName.sendKeys("username");
+//		pswd.sendKeys("password");
+//		loginBtn1.click();
+//
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//
+//  		driver.quit();
+//	}
+}
