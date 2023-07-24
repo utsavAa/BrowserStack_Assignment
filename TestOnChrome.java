fb05d85d34bdd631a002705bf13a5c6d9f31e3a9
Utsav Banerjee <42199813+utsavAa@users.noreply.github.com>
2023-07-24 21:15:00 +0530

    changed

--- TestOnChrome.java
+++ TestOnChrome.java
@@ -12,8 +12,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestOnChrome {
 
 	public static void main(String[] args){
-		// String username = System.getenv("USERNAME");
-        // String password = System.getenv("PASSWORD");
+		String username = System.getenv("USERNAME");
+        String password = System.getenv("PASSWORD");
 		System.setProperty("webdriver.chrome.driver", "/Users/utsavbanerjee/Downloads/chromedriver 2");
 
 		//Creating WebDriver instance
